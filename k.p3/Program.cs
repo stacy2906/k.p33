9b83767fcfeeb43dead1063197662350c00b6858
stacy2906 <anastasiya.tkachyk.290606@gmail.com>
2023-02-28 19:12:13 +0200

    Create Program.cs
    add k.p3

---
+++ k.p3/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace k.p3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { //Задание 1 Заполнить одномерный массив с клавиатуры и вывести его на экран консоли с использованием цикла for.
+          //Console.Write("n=");
+          //int n=Convert.ToInt32(Console.ReadLine());
+          //int[] mass = new int[n];
+          //for (int i = 0; i < n; i++)
+          //{
+          // Console.Write($"mass[{i}]=");
+          // mass[i]=Convert.ToInt32(Console.ReadLine());
+          //}
+          //for (int i = 0; i < n; i++)
+          //{
+          // Console.WriteLine($"mass[{i}]={mass[i]}");
+          //}
+
+            //Задание 2 Заполнить одномерный массив случайным образом и вывести его на экран с использованием цикла foreach.
+            //Console.Write("n=");
+            //int n=Convert.ToInt32(Console.ReadLine());
+            //int[] mass = new int[n];
+            //Random random = new Random();
+            //for (int i = 0; i < n; i++)
+            //{
+            // mass[i] = random.Next(-10,10);
+            //}
+            //foreach(int elem in mass)
+            //{
+            // Console.WriteLine($"{elem}");
+            //}
+
+            //Задача 3 В одномерном массиве, заполненном с использованием явной инициализации, найти:
+            //1. максимальный элемент и его индекс;
+            //2.минимальный элемент и его индекс;
+            //3.поменять местами максимальный и минимальный элементы, вывести одномерный массив;
+            //4.количество элементов между максимальным и минимальным элементами.
+            
+            //Console.Write("n=");
+            //int n=Convert.ToInt32(Console.ReadLine());
+            //int[] mass = new int[n];
+            //for(int i=0;i<n;i++)
+            //{
+            // Console.Write($"mass[{i}]=");
+            // mass[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+            //int max = mass[0];
+            //int index1 = 0;
+            //int min = mass[0];
+            //int index2 = 0;
+            //for(int i=0;i<n;i++)
+            //{
+            //    if (mass[i]>max)
+            //    {
+            //        max= mass[i];
+            //        index1 = i;
+            //    }
+            //    if(mass[i]<min)
+            //    {
+            //        min= mass[i];
+            //        index2 = i;
+            //    }
+            //}
+            //Console.WriteLine($"max={max}");
+            //Console.WriteLine($"index1={index1}");
+            //Console.WriteLine($"min={min}");
+            //Console.WriteLine($"index2={index2}");
+            //int new_min = max;
+            //int new_max = min;
+            //for(int i=0;i<n;i++)
+            //{
+            //    Console.WriteLine($"mass[{i}]={mass[i]}");
+            //}
+            //Console.WriteLine($"new_max={new_max}");
+            //Console.WriteLine($"new_min={new_min}");
+            //Console.WriteLine($"Количество элементов между максимумом и минимумом={Math.Abs(index2-index1)-1}");
+            Console.ReadKey();
+        }
+    }
+}
